ac1b4db64dc745b6a99a29c5cc268c7c9e6deea1
vin123vin123 <vin33666@gmail.com>
2026-08-09 15:06:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -4,7 +4,9 @@ const bcrypt = require('bcryptjs');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+
+mongoose.connect(process.env.MONGO_URI)
+
 
 // Middleware
 app.use(express.json());
